262244efb530de5b333a392469e1ea63e22acb2f
Merlijn Vanherck <merlijnvanherck@gmail.com>
2025-08-01 07:04:29 +0200

    1979 find greatest common divisor of array

---
+++ 1979-find-greatest-common-divisor-of-array.ts
@@ -0,0 +1,10 @@
+function findGCD(nums: number[]): number {
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+
+    for (let i = min; i > 1; i--)
+        if (min % i === 0 && max % i === 0)
+            return i;
+
+    return 1;
+};
